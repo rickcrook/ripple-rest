69abf0fd036020dd559a25d76cbdf49af5798c70
Rick Crook <richard@pinkgrass.org>
2015-01-18 16:34:42 +0000

    fix for server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ logger.info('server.js starting up');
 host = process.env.OPENSHIFT_NODEJS_IP;
 port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-if (typeof self.ipaddress === "undefined") {
+if (typeof host === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    self.ipaddress = "127.0.0.1";
+    host = "127.0.0.1";
 };
 
 logger.info('Openshift port:' + port);
