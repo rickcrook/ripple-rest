f2ff48473d8636cf7c28e307b3e76e5597346373
Rick Crook <richard@pinkgrass.org>
2015-01-18 16:29:38 +0000

    server.js openshifted

--- server.js
+++ server.js
@@ -1,159 +1,77 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+var fs        = require('fs');
+var https     = require('https');
+var constants = require('constants')
+var app       = require('./lib/express_app.js');
+var config    = require('./lib/config');
+var logger    = require('./lib/logger.js').logger;
+var utils     = require('./lib/utils.js');
 
+//Ignore config and use Openshift variables
+//var port = config.get('port') || 5990;
+//var host = config.get('host');
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+logger.info('server.js starting up');
 
-    //  Scope.
-    var self = this;
+//  Set the environment variables we need
+host = process.env.OPENSHIFT_NODEJS_IP;
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+if (typeof self.ipaddress === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+    self.ipaddress = "127.0.0.1";
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+logger.info('Openshift port:' + port);
+logger.info('Openshift host:' + host);
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+logger.info('ripple-rest (v' + utils.getPackageVersion() + ')');
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+function loadSSLConfig() {
+  var keyPath  = config.get('ssl').key_path || './certs/server.key';
+  var certPath = config.get('ssl').cert_path || './certs/server.crt';
 
+  if (!fs.existsSync(keyPath)) {
+    logger.error('Must specify key_path in order to use SSL');
+    process.exit(1);
+  }
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+  if (!fs.existsSync(certPath)) {
+    logger.error('Must specify cert_path in order to use SSL');
+    process.exit(1);
+  }
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+  return {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
 
+    //
+    // Protecting against POODLE in node.js
+    // source: https://gist.github.com/3rd-Eden/715522f6950044da45d8
+    //
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
+    //
+    // This is the default secureProtocol used by Node.js, but it might be
+    // sane to specify this by default as it's required if you want to
+    // remove supported protocols from the list. This protocol supports:
+    //
+    // - SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2
+    //
+    secureProtocol: 'SSLv23_method',
 
+    //
+    // Supply `SSL_OP_NO_SSLv3` constant as secureOption to disable SSLv3
+    // from the list of supported protocols that SSLv23_method supports.
+    secureOptions: constants.SSL_OP_NO_SSLv3
+  };
+};
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+if (config.get('ssl_enabled')) {
+  require('https').createServer(loadSSLConfig(), app).listen(port, host, function() {
+    logger.info('server listening over HTTPS at port ' + port);
+  });
+} else {
+  app.listen(port, host, function() {
+    logger.info('server listening over UNSECURED HTTP at port ' + port);
+  });
+}
